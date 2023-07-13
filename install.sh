e164a943b14c5b2237a199148df356b576dcd704
yuki cui <71556325+yuqingcuiyuki@users.noreply.github.com>
2023-07-13 08:51:40 -0400

    change permission

---
+++ install.sh
@@ -0,0 +1,14 @@
+
+
+pip install --upgrade pip
+pip install informationtracer
+pip install mysql-connector-python
+pip install pandas
+pip install numpy
+pip install pysentimiento
+pip install nltk
+pip install wordcloud
+
+pip install transformers[torch]
+pip install accelerate -U
+
